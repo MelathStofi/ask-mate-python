57f371a58f1ab3c8af15b8b691b689979203d5c0
Melath Stofi <melath.stofi@gmail.com>
2019-05-21 14:24:08 +0200

    some cleaning according to pep8

--- connection.py
+++ connection.py
@@ -1,8 +1,15 @@
 import csv
 
 
-def get_all_data():
+def get_all_questions():
     with open('sample_data/question.csv', "r") as file:
         data = list(csv.DictReader(file))
 
     return data
+
+
+def write_question_to_file(questions, header):  # questions: name of the list with dictionaries; header: list of headers
+    with open("sample_data/question.csv", "w") as file:
+        csv_writer = csv.DictWriter(file, fieldnames=header, delimiter=',')
+        csv_writer.writeheader()
+        csv_writer.writerows(questions)
--- server.py
+++ server.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    every_questions = connection.get_all_data()
+    every_questions = connection.get_all_questions()
 
     return render_template('list.html', every_questions=every_questions)
-
 
 
 @app.route('/question/<question_id>')
 def display_question(question_id):
     return render_template('question.html', question_id =question_id)
+
 
 @app.route('/add-question', methods=['GET', 'POST'])
 def route_add_question():
@@ -29,7 +29,7 @@ def route_add_question():
 def route_post_answer(question_id):
     if request.method == 'POST':
         return redirect("/list")
-    return render_template("post_answer.html")
+    return render_template("post_answer.html", question_id=question_id)
 
 
 if __name__ == '__main__':
