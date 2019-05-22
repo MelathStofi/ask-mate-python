11939e30dd939f0937b4e1db613c32490a45d259
Melath Stofi <melath.stofi@gmail.com>
2019-05-22 13:28:11 +0200

    Displaying answers works

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect, request
 import data_manager
-import connection
 
 app = Flask(__name__)
 
@@ -8,28 +7,27 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    every_questions = connection.get_all_questions()
-
-    return render_template('list.html', every_questions=every_questions)
+    every_questions = data_manager.get_all_questions()
+    return render_template('list.html',
+                           every_questions=every_questions)
 
 
 @app.route('/question/<question_id>')
 def display_question(question_id):
     question = data_manager.get_question_by_id(question_id)
-
-    return render_template('question.html', question=question)
+    answers = data_manager.get_answers_by_id(question_id)
+    return render_template('question.html',
+                           question=question,
+                           answers=answers)
 
 
 @app.route('/add-question', methods=['GET', 'POST'])
 def route_add_question():
     if request.method == 'POST':
-        question = {
-            'title': request.form['title'],
-            'message': request.form['message']
-        }
+        question = {'title': request.form['title'],
+                    'message': request.form['message']}
         data_manager.add_question(question)
         return redirect('/list')
-
     return render_template('add_question.html')
 
 
@@ -37,7 +35,8 @@ def route_add_question():
 def route_post_answer(question_id):
     if request.method == 'POST':
         return redirect("/list")
-    return render_template("post_answer.html", question_id=question_id)
+    return render_template("post_answer.html",
+                           question_id=question_id)
 
 
 if __name__ == '__main__':
