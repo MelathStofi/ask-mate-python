469dd1ed12dfe8e9c98e88bfc9dc58600828dad0
GyorgySandor <sgyorgy91@gmail.com>
2019-06-04 13:51:35 +0200

    CSV changed to database connection list.html lists the questions

--- data_manager.py
+++ data_manager.py
@@ -8,123 +8,130 @@ answers_data = "sample_data/answer.csv"
 header = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 
 
-def get_all_questions():
-    return connection.get_all_data(questions_data)
+@connection.connection_handler
+def get_all_questions(cursor):
+    cursor.execute("""
+                        SELECT * FROM question;
+                        
+                       """)
+    questions = cursor.fetchall()
+    return questions
 
 
-def get_question_by_id(_id):
-    questions = connection.get_all_data(questions_data)
-    for question in questions:
-        if question['id'] == _id:
-            return question
 
-
-def get_answers_by_id(_id):
-    all_answers = connection.get_all_data(answers_data)
-    answers_by_id = []
-    for answer in all_answers:
-        if answer['question_id'] == _id:
-            answers_by_id.append(answer)
-    return answers_by_id
-
-
-def add_question(question):
-    all_questions = connection.get_all_data(questions_data)
-    question['id'] = len(all_questions) + 1
-    question['submission_time'] = timestamp
-    all_questions.append(question)
-    connection.write_data_to_file(questions_data, all_questions, header)
-
-
-def add_answer(answer, question_id):
-    answer_header = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
-    all_answers = connection.get_all_data(answers_data)
-    answer['id'] = len(all_answers) + 1
-    answer['question_id'] = question_id
-    answer['submission_time'] = timestamp
-    all_answers.append(answer)
-    connection.write_data_to_file(answers_data, all_answers, answer_header)
-
-
-def count_data_lines(data_file):
-    all_data = connection.get_all_data(data_file)
-    for i in range(len(all_data)):
-        data_lines = i
-        return data_lines
-
-
-def update_story(updated_question):
-    questions = get_all_questions()
-
-    for row in range(len(questions)):
-        if questions[row]['id'] == updated_question['id']:
-            questions[row]['title'] = updated_question['title']
-            questions[row]['message'] = updated_question['message']
-            questions[row]['image'] = updated_question['image']
-    return connection.write_data_to_file('sample_data/question.csv', questions, header)
-
-
-def get_data_row(row_id):
-    questions = get_all_questions()
-    for row in range(len(questions)):
-        if questions[row]['id'] == row_id:
-            return questions[row]
-
-
-def voting(question_id, vote_act):
-    v = 0
-    questions = get_all_questions()
-    for question in questions:
-        if question["id"] == question_id:
-            v = question['vote_number']
-            try:
-                v = int(v)
-            except:
-                pass
-            if vote_act == 1:
-                v += 1
-            elif vote_act == -1:
-                if v == 0:
-                    pass
-                else:
-                    v -= 1
-        question['vote_number'] = str(v)
-
-    return connection.write_data_to_file(questions_data, questions, header)
-
-
-def count_views(question_id, increment):
-    view = 0
-    questions = get_all_questions()
-    for question in questions:
-        if question["id"] == question_id:
-            view = question['view_number']
-            try:
-                view = int(view)
-            except:
-                view = 0
-            view += increment
-            question["view_number"] = str(view)
-
-    return connection.write_data_to_file(questions_data, questions, header)
-
-
-def sorting_table(order_by=None, order_in=None):
-    print(itemgetter(order_by))
-    questions = connection.get_all_data(questions_data)
-    if order_by is None and order_in is None:
-        return questions
-    elif order_by == 'title':
-        if order_in == "desc":
-            sorted_questions = sorted(questions, key=lambda k: k[order_by].lower(), reverse=False)
-            return sorted_questions
-        elif order_in == "asc":
-            sorted_questions = sorted(questions, key=lambda k: k[order_by].lower(), reverse=True)
-            return sorted_questions
-    elif order_by == 'submission_time' or order_by == 'view_number' or order_by == 'vote_number':
-        if order_in == "desc":
-            sorted_questions = sorted(questions, key=lambda k: int(k[order_by]), reverse=False)
-            return sorted_questions
-        elif order_in == "asc":
-            sorted_questions = sorted(questions, key=lambda k: int(k[order_by]), reverse=True)
-            return sorted_questions
+# def get_question_by_id(_id):
+#     questions = connection.get_all_data(questions_data)
+#     for question in questions:
+#         if question['id'] == _id:
+#             return question
+#
+#
+# def get_answers_by_id(_id):
+#     all_answers = connection.get_all_data(answers_data)
+#     answers_by_id = []
+#     for answer in all_answers:
+#         if answer['question_id'] == _id:
+#             answers_by_id.append(answer)
+#     return answers_by_id
+#
+#
+# def add_question(question):
+#     all_questions = connection.get_all_data(questions_data)
+#     question['id'] = len(all_questions) + 1
+#     question['submission_time'] = timestamp
+#     all_questions.append(question)
+#     connection.write_data_to_file(questions_data, all_questions, header)
+#
+#
+# def add_answer(answer, question_id):
+#     answer_header = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+#     all_answers = connection.get_all_data(answers_data)
+#     answer['id'] = len(all_answers) + 1
+#     answer['question_id'] = question_id
+#     answer['submission_time'] = timestamp
+#     all_answers.append(answer)
+#     connection.write_data_to_file(answers_data, all_answers, answer_header)
+#
+#
+# def count_data_lines(data_file):
+#     all_data = connection.get_all_data(data_file)
+#     for i in range(len(all_data)):
+#         data_lines = i
+#         return data_lines
+#
+#
+# def update_story(updated_question):
+#     questions = get_all_questions()
+#
+#     for row in range(len(questions)):
+#         if questions[row]['id'] == updated_question['id']:
+#             questions[row]['title'] = updated_question['title']
+#             questions[row]['message'] = updated_question['message']
+#             questions[row]['image'] = updated_question['image']
+#     return connection.write_data_to_file('sample_data/question.csv', questions, header)
+#
+#
+# def get_data_row(row_id):
+#     questions = get_all_questions()
+#     for row in range(len(questions)):
+#         if questions[row]['id'] == row_id:
+#             return questions[row]
+#
+#
+# def voting(question_id, vote_act):
+#     v = 0
+#     questions = get_all_questions()
+#     for question in questions:
+#         if question["id"] == question_id:
+#             v = question['vote_number']
+#             try:
+#                 v = int(v)
+#             except:
+#                 pass
+#             if vote_act == 1:
+#                 v += 1
+#             elif vote_act == -1:
+#                 if v == 0:
+#                     pass
+#                 else:
+#                     v -= 1
+#         question['vote_number'] = str(v)
+#
+#     return connection.write_data_to_file(questions_data, questions, header)
+#
+#
+# def count_views(question_id, increment):
+#     view = 0
+#     questions = get_all_questions()
+#     for question in questions:
+#         if question["id"] == question_id:
+#             view = question['view_number']
+#             try:
+#                 view = int(view)
+#             except:
+#                 view = 0
+#             view += increment
+#             question["view_number"] = str(view)
+#
+#     return connection.write_data_to_file(questions_data, questions, header)
+#
+#
+# def sorting_table(order_by=None, order_in=None):
+#     print(itemgetter(order_by))
+#     questions = connection.get_all_data(questions_data)
+#     if order_by is None and order_in is None:
+#         return questions
+#     elif order_by == 'title':
+#         if order_in == "desc":
+#             sorted_questions = sorted(questions, key=lambda k: k[order_by].lower(), reverse=False)
+#             return sorted_questions
+#         elif order_in == "asc":
+#             sorted_questions = sorted(questions, key=lambda k: k[order_by].lower(), reverse=True)
+#             return sorted_questions
+#     elif order_by == 'submission_time' or order_by == 'view_number' or order_by == 'vote_number':
+#         if order_in == "desc":
+#             sorted_questions = sorted(questions, key=lambda k: int(k[order_by]), reverse=False)
+#             return sorted_questions
+#         elif order_in == "asc":
+#             sorted_questions = sorted(questions, key=lambda k: int(k[order_by]), reverse=True)
+#             return sorted_questions
--- server.py
+++ server.py
@@ -7,72 +7,73 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list', methods=['GET'])
 def route_list():
-    order_by = request.args.get('order_by')
-    order_in = request.args.get('order_in')
-    every_question = data_manager.sorting_table(order_by, order_in)
+    # order_by = request.args.get('order_by')
+    # order_in = request.args.get('order_in')
+    # need to call data_manager.sorting if implemented
+    every_question = data_manager.get_all_questions()
     print(every_question)
     return render_template('list.html', every_question=every_question)
 
 
-@app.route('/question/<question_id>', methods=['GET', 'POST'])
-def route_display_question(question_id):
-    question = data_manager.get_question_by_id(question_id)
-    answers = data_manager.get_answers_by_id(question_id)
-    data_manager.count_views(question_id, 1)
-    return render_template('question.html',
-                           question=question,
-                           answers=answers)
-
-
-@app.route('/add-question', methods=['GET', 'POST'])
-def route_add_question():
-    if request.method == 'POST':
-        question = {'view_number': "0",
-                    'vote_number': "0",
-                    'title': request.form['title'],
-                    'message': request.form['message'],
-                    'image': request.form['image']}
-        data_manager.add_question(question)
-        return redirect('/list')
-    return render_template('add_question.html')
-
-
-@app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
-def route_add_answer(question_id):
-    if request.method == 'POST':
-        answer = {'message': request.form['answer'],
-                  'image': request.form['image']}
-        data_manager.add_answer(answer, question_id)
-        return redirect("/list")
-    return render_template("add_answer.html",
-                           question_id=question_id)
-
-
-@app.route("/question/<question_id>/vote-up", methods=["GET", "POST"])
-@app.route("/question/<question_id>/vote-down", methods=["GET", "POST"])
-def route_voting(question_id):
-    question = data_manager.get_question_by_id(question_id)
-    if 'vote-up' in str(request.url_rule):
-        data_manager.voting(question_id, 1)
-    elif 'vote-down' in str(request.url_rule):
-        data_manager.voting(question_id, -1)
-    data_manager.count_views(question_id, -1)
-    return redirect(url_for("route_display_question", question_id=question['id']))
-
-
-@app.route('/edit/<question_id>', methods=['GET','POST'])
-def route_update_question(question_id):
-    if request.method == 'POST':
-        updated_question = {'id': question_id}
-
-        updated_question.update(request.form)
-        data_manager.update_story(updated_question)
-        return redirect('/')
-
-    update_question_row = data_manager.get_data_row(question_id)
-    return render_template('edit.html',
-                           question_id=question_id,
-                           update_question_row=update_question_row)
+# @app.route('/question/<question_id>', methods=['GET', 'POST'])
+# def route_display_question(question_id):
+#     question = data_manager.get_question_by_id(question_id)
+#     answers = data_manager.get_answers_by_id(question_id)
+#     data_manager.count_views(question_id, 1)
+#     return render_template('question.html',
+#                            question=question,
+#                            answers=answers)
+#
+#
+# @app.route('/add-question', methods=['GET', 'POST'])
+# def route_add_question():
+#     if request.method == 'POST':
+#         question = {'view_number': "0",
+#                     'vote_number': "0",
+#                     'title': request.form['title'],
+#                     'message': request.form['message'],
+#                     'image': request.form['image']}
+#         data_manager.add_question(question)
+#         return redirect('/list')
+#     return render_template('add_question.html')
+#
+#
+# @app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
+# def route_add_answer(question_id):
+#     if request.method == 'POST':
+#         answer = {'message': request.form['answer'],
+#                   'image': request.form['image']}
+#         data_manager.add_answer(answer, question_id)
+#         return redirect("/list")
+#     return render_template("add_answer.html",
+#                            question_id=question_id)
+#
+#
+# @app.route("/question/<question_id>/vote-up", methods=["GET", "POST"])
+# @app.route("/question/<question_id>/vote-down", methods=["GET", "POST"])
+# def route_voting(question_id):
+#     question = data_manager.get_question_by_id(question_id)
+#     if 'vote-up' in str(request.url_rule):
+#         data_manager.voting(question_id, 1)
+#     elif 'vote-down' in str(request.url_rule):
+#         data_manager.voting(question_id, -1)
+#     data_manager.count_views(question_id, -1)
+#     return redirect(url_for("route_display_question", question_id=question['id']))
+#
+#
+# @app.route('/edit/<question_id>', methods=['GET','POST'])
+# def route_update_question(question_id):
+#     if request.method == 'POST':
+#         updated_question = {'id': question_id}
+#
+#         updated_question.update(request.form)
+#         data_manager.update_story(updated_question)
+#         return redirect('/')
+#
+#     update_question_row = data_manager.get_data_row(question_id)
+#     return render_template('edit.html',
+#                            question_id=question_id,
+#                            update_question_row=update_question_row)
 
 
 if __name__ == '__main__':
