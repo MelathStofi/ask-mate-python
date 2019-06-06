acc58bef9e0e1ba4dfbb942d724708f09fb78acf
Gergő Zelei <zeleig96@gmail.com>
2019-06-06 14:55:10 +0200

    Redirecting is working on every page now

--- server.py
+++ server.py
@@ -54,7 +54,7 @@ def route_add_answer(question_id):
         answer = {'message': request.form['answer'],
                   'image': request.form['image']}
         data_manager.add_answer(answer, question_id)
-        return redirect("/")
+        return redirect(f"/question/{question_id}")
     return render_template("add_answer.html",
                             question_id = question_id)
 
@@ -66,7 +66,8 @@ def edit_answer(answer_id):
                   'message': request.form['answer'],
                   'image': request.form['image']}
         data_manager.update_answer(answer,answer_id)
-        return redirect('/')
+        answer_row = data_manager.get_answer_row(answer_id)
+        return redirect(f'/question/{answer_row["question_id"]}')
     answer_row = data_manager.get_answer_row(answer_id)
     return render_template("update_answer.html",answer_id=answer_id,answer_row=answer_row)
 
@@ -75,8 +76,6 @@ def edit_answer(answer_id):
 @app.route("/question/<question_id>/vote-down", methods=["GET", "POST"])
 def route_voting(question_id):
     question = data_manager.get_question_by_id(question_id)
-    print(request.url)
-    print(request.referrer)
     if 'vote-up' in str(request.url_rule):
         data_manager.voting(question_id, 1)
     elif 'vote-down' in str(request.url_rule):
@@ -90,7 +89,7 @@ def route_update_question(question_id):
         updated_question = {'id': question_id}
         updated_question.update(request.form)
         data_manager.update_story(updated_question,question_id)
-        return redirect('/')
+        return redirect(f'/question/{question_id}')
     update_question_row = data_manager.get_data_row(question_id)
     return render_template('edit.html', question_id=question_id, update_question_row=update_question_row)
 
@@ -103,8 +102,9 @@ def delete_question(question_id):
 
 @app.route('/delete_answer/<answer_id>')
 def delete_answer(answer_id):
+    answer = data_manager.get_answer_row(answer_id)
     data_manager.delete_answer(answer_id)
-    return redirect('/')
+    return redirect(f'/question/{answer["question_id"]}')
 
 
 if __name__ == '__main__':
